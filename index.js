c4f83e4501bbc5adbf61c6636838ff829bab6621
collins otieno <collins.ouma2@student.moringaschool.com>
2025-06-08 13:35:49 +0300

    first class

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(driversArray) {
+  return driversArray.slice(0, 2); 
+}
+function returnLastTwoDrivers(driversArray){
+    return driversArray.slice(-2)
+}
+const selectingDrivers = [
+  returnFirstTwoDrivers, 
+  returnLastTwoDrivers,  
+];
+function createFareMultiplier(multiplier) {
+  return function(value) {
+    return value * multiplier;
+  };
+}
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(arrayOfDrivers, selectionFunction) {
+  return selectionFunction(arrayOfDrivers);
+}
